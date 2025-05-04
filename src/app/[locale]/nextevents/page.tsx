e9caa10def7a6fb511481ff1fe1efb22e1a9b5f8
Loris Caputo <46825967+loris1712@users.noreply.github.com>
2025-05-04 10:27:06 +0200

    CHanges

--- src/app/[locale]/nextevents/page.tsx
+++ src/app/[locale]/nextevents/page.tsx
@@ -28,6 +28,8 @@ export default function Events() {
     { id: 15, name: "Cleope at Caffe Pascucci Ibiza", date: "3rd May 2025", type: "Pascucci Ibiza 03.05", special: true },
     { id: 16, name: "CLEOPE x Tantra Ibiza", date: "4th May 2025", type: "Tantra Ibiza 04.05", special: true },
     { id: 17, name: "CLEOPE X BLUE GROOVE ARCA Milano", date: "10th May 2025", type: "standard", ticket: "https://dice.fm/event/pyexrl-cleope-x-blue-groove-10th-may-arca-milano-tickets" },
+    { id: 18, name: "Afrodite x CLEOPE Downtown Milano", date: "10th May 2025", type: "Downtown 10.05", special: true },
+    { id: 19, name: "CLEOPE x VOLT Club Milan", date: "8th\ May 2025", type: "Volt 08.05", special: true },
 
 
   ];
