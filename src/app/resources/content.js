ba48da8ab7e8c62213de8b94033277b02bafc6a0
Loris Caputo <46825967+loris1712@users.noreply.github.com>
2025-05-12 22:50:57 +0200

    Changes

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -42,7 +42,7 @@ const home = {
     label: 'Home',
     title: `CLEOPE`,
     description: `CLEOPE - Fashion Party`,
-    headline: <>Cleope x Cvlture - 19th April 2025</>,
+    headline: <>Next Fashion Partyc ??</>,
     subline: <>CLEOPE - Fashion Party | Night Party. Life is too short to be mass approved.</>
 }
 
