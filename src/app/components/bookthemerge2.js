4ec9de1099f895e82549b6677237c571ba48c704
Loris Caputo <l.caputo@quest-it.com>
2026-02-11 12:07:00 +0100

    Changes

--- src/app/components/bookthemerge2.js
+++ src/app/components/bookthemerge2.js
@@ -186,7 +186,7 @@ if (loading) {
         <img
           src="/images/themerge2/header.png"
           alt="The Merge"
-          className="md:w-[25vw] w-[90vw] mb-4 opacity-90"
+          className="md:w-[25vw] w-[60vw] mb-4 opacity-90"
         />
         <p className="micro mb-4">28 · 02 · 2026</p>
 
